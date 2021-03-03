44778755ed28a0a91960df71bf94b97890cebc18
natanaeladitya <natanaela@xrvision.ai>
2021-03-03 14:39:37 +0800

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace videojs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
